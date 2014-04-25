d8490d5ac6f82119ea463b08f435b6803e915b46
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2014-04-25 22:41:30 +0100

    end-to-end on opening a connection

--- src/asynqp/__init__.py
+++ src/asynqp/__init__.py
@@ -10,22 +10,41 @@ def write_protocol_header(writer):
     writer.write(b'AMQP\x00\x00\x09\x01')
 
 
-def write_start_ok_message(writer, username, password):
-    payload_builder = PayloadBuilder(MethodType.connection_start_ok)
+def handle_connection_start(writer, frame, username, password):
+    builder = PayloadBuilder(MethodType.connection_start_ok)
 
     client_properties = {}
-    payload_builder.add_table(client_properties)
+    builder.add_table(client_properties)
 
     mechanism = 'AMQPLAIN'
-    payload_builder.add_short_string(mechanism)
+    builder.add_short_string(mechanism)
 
     security_response = {'LOGIN': username, 'PASSWORD': password}
-    payload_builder.add_table(security_response)
+    builder.add_table(security_response)
 
     locale = 'en_US'
-    payload_builder.add_short_string(locale)
+    builder.add_short_string(locale)
 
-    frame = Frame(FrameType.method, 0, payload_builder.build())
+    frame = Frame(FrameType.method, 0, builder.build())
+    write_frame(writer, frame)
+
+
+def handle_tune(writer, frame):
+    builder = PayloadBuilder(MethodType.connection_tune_ok)
+
+    builder.add_short(1024)  # maximum channel number
+    builder.add_long(1024 * 128)  # 128 kb
+    builder.add_short(600)  # no heartbeat
+
+    return_frame = Frame(FrameType.method, 0, builder.build())
+    write_frame(writer, return_frame)
+
+
+    builder = PayloadBuilder(MethodType.connection_open)
+    builder.add_short_string('/')  # virtual host
+    builder.add_short_string('')
+    builder.add_bit(False)
+    frame = Frame(FrameType.method, 0, builder.build())
     write_frame(writer, frame)
 
 
@@ -37,7 +56,6 @@ def write_frame(writer, frame):
 def read_frame(reader):
     bytes = yield from reader.read(7)
     frame_type, channel_id, size = struct.unpack('!BHL', bytes)
-
     raw_payload = yield from reader.read(size)
     frame_end = yield from reader.read(1)
     if frame_end !=  b'\xCE':
@@ -57,7 +75,7 @@ class Frame(object):
     def serialise(self):
         payload = self.payload.serialise()
         frame = pack_octet(self.frame_type.value)
-        frame += pack_int(self.channel_id)
+        frame += pack_short(self.channel_id)
         frame += self.payload.serialise()
         frame += FRAME_END  # frame_end
         return frame
@@ -81,7 +99,9 @@ class MethodType(enum.Enum):
     connection_start = (10, 10)
     connection_start_ok = (10, 11)
     connection_tune = (10, 30)
-
+    connection_tune_ok = (10, 31)
+    connection_open = (10, 40)
+    connection_open_ok = (10, 41)
 
 class AMQPError(IOError):
     pass
@@ -91,15 +111,46 @@ class PayloadBuilder(object):
     def __init__(self, method_type):
         self.method_type = method_type
         self.body = b''
+        self.bits = []
+        self.bitcount = 0
+
+    def add_bit(self, b):
+        b = 1 if b else 0
+        shift = self.bitcount % 8
+        if shift == 0:
+            self.bits.append(0)
+        self.bits[-1] |= (b << shift)
+        self.bitcount += 1
+
+    def add_octet(self, number):
+        self._flush_bits()
+        self.body += pack_octet(number)
+
+    def add_short(self, number):
+        self._flush_bits()
+        self.body += pack_short(number)
+
+    def add_long(self, number):
+        self._flush_bits()
+        self.body += pack_long(number)
 
     def add_short_string(self, string):
+        self._flush_bits()
         self.body += short_string(string)
 
     def add_table(self, d):
+        self._flush_bits()
         self.body += table(d)
 
     def build(self):
+        self._flush_bits()
         return Payload(self.method_type, self.body)
+
+    def _flush_bits(self):
+        for b in self.bits:
+            self.body += struct.pack('B', b)
+        self.bits = []
+        self.bitcount = 0
 
 
 def short_string(string):
@@ -121,7 +172,7 @@ def table(d):
 def pack_octet(number):
     return struct.pack('!B', number)
 
-def pack_int(number):
+def pack_short(number):
     return struct.pack('!H', number)
 
 def pack_long(number):
--- test/test.py
+++ test/test.py
@@ -31,19 +31,39 @@ class WhenOpeningAConnection(StreamConnectionContext):
 
 
 class WhenRespondingToConnectionStart(StreamConnectionContext):
-    def given_that_the_server_is_awaiting_start_ok(self):
+    def given_a_start_frame_from_the_server(self):
         asynqp.write_protocol_header(self.writer)
-        self.loop.run_until_complete(asynqp.read_frame(self.reader))
+        self.start_frame = self.loop.run_until_complete(asynqp.read_frame(self.reader))
 
-    def because_we_write_a_start_ok_frame(self):
-        asynqp.write_start_ok_message(self.writer, 'guest', 'guest')
-        self.frame = self.loop.run_until_complete(asynqp.read_frame(self.reader))
+    def because_the_start_frame_arrives(self):
+        asynqp.handle_connection_start(self.writer, self.start_frame, 'guest', 'guest')  # TODO: what does the server do when the credentials are wrong?
+        self.response = self.loop.run_until_complete(asynqp.read_frame(self.reader))
 
     def it_should_return_a_method_frame(self):
-        assert self.frame.frame_type == asynqp.FrameType.method
+        assert self.response.frame_type == asynqp.FrameType.method
 
     def it_should_be_communicating_on_the_default_channel(self):
-        assert self.frame.channel_id == 0
+        assert self.response.channel_id == 0
 
     def it_should_be_a_tune_method(self):
-        assert self.frame.payload.method_type == asynqp.MethodType.connection_tune
+        assert self.response.payload.method_type == asynqp.MethodType.connection_tune
+
+
+class WhenRespondingToConnectionTune(StreamConnectionContext):
+    def given_a_tune_frame_from_the_server(self):
+        self.tune_frame = self.loop.run_until_complete(self.get_tune_frame())
+
+    def when_the_tune_frame_arrives(self):
+        asynqp.handle_tune(self.writer, self.tune_frame)
+        self.response = self.loop.run_until_complete(asyncio.wait_for(asynqp.read_frame(self.reader), 0.1))
+
+    def it_should_return_open_ok(self):
+        assert self.response.payload.method_type == asynqp.MethodType.connection_open_ok
+
+    @asyncio.coroutine
+    def get_tune_frame(self):
+        asynqp.write_protocol_header(self.writer)
+        start_frame = yield from asynqp.read_frame(self.reader)
+        asynqp.handle_connection_start(self.writer, start_frame, 'guest', 'guest')
+        tune_frame = yield from asynqp.read_frame(self.reader)
+        return tune_frame
