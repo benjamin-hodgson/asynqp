9afc4bf75f45a62602b2b249e035dcd421a1dbec
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2014-04-27 18:12:52 +0100

    forgot to include the xml file

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, find_packages
 setup(
 	name = 'asynqp',
 	packages = find_packages('src'),
-	package_dir = {'': 'src'}
+	package_dir = {'': 'src'},
+    package_data = {'asynqp': ['amqp0-9-1.xml']}
 )
