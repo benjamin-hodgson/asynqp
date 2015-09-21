443836782f44470b2577c5cadb435e9c76cf9331
Taras <taras@doba.ua>
2015-09-21 21:36:34 +0300

    Log warning if called `close` on already closed connection

--- src/asynqp/connection.py
+++ src/asynqp/connection.py
@@ -1,8 +1,11 @@
 import asyncio
+import logging
 import sys
 from . import channel
 from . import spec
 from . import routing
+
+log = logging.getLogger(__name__)
 
 
 class Connection(object):
@@ -70,6 +73,8 @@ class Connection(object):
             self.protocol.heartbeat_monitor.stop()
             yield from self.protocol.heartbeat_monitor.wait_closed()
             self.closed.set_result(True)
+        else:
+            log.warn("Called `close` on already closed connection...")
 
 
 @asyncio.coroutine
