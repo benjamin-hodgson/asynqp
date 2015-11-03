5fbde91f640b81485b3b594d99f979e03dcd1d51
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2015-11-03 11:39:22 +0000

    mute log in tests

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,10 @@
 import asyncio
+import logging
 from .util import testing_exception_handler
 
 
 loop = asyncio.get_event_loop()
 loop.set_exception_handler(testing_exception_handler)
+
+
+logging.getLogger('asynqp').setLevel(100)  # mute the logger
